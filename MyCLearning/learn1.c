4de8433d226e2569027279a43ff96bf39576ee00
q77889966 <q77889966@DESKTOP-FE36LI4>
2021-08-03 10:44:00 +0800

    添加项目文件。

---
+++ MyCLearning/learn1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//%d int short int long
+// %c char
+// %f float double 小数
+// %p 地址
+// %x 16进制
+
+
+
+
+//int main()
+//{
+//	printf("hello world!!\n");
+//	return 0;
+//}
+
+
+
+
+int main()
+{
+	//char ch = 'A';
+	//printf("%c", ch);
+	//short int age = 20;
+
+
+
+	//printf("%d\n", age);
+	//return 0;
+}
