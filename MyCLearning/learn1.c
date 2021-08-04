c1ff9c15569b82d51372d3a0e8f9b251cdc0b843
q77889966 <q77889966@DESKTOP-FE36LI4>
2021-08-04 11:40:24 +0800

    11

--- MyCLearning/learn1.c
+++ MyCLearning/learn1.c
@@ -14,17 +14,38 @@
 //	return 0;
 //}
 
-
-
-
+//int num2 = 20;//全局变量
 int main()
 {
 	//char ch = 'A';
 	//printf("%c", ch);
 	//short int age = 20;
+	//int sum = 0;
+	//int num1 = 0;
+	//int num2 = 0;
 
-
+	//scanf("%d%d", &num1, &num2); //取地址&
+	//sum = num1 + num2;
+	//printf("sum = %d\n",sum);
+	//return 0;
+	//int num1 = 10;//局部变量
 
 	//printf("%d\n", age);
 	//return 0;
+
+	//printf("%d\n", sizeof(char));
+	//printf("%d\n", sizeof(short));
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof(long));
+	//printf("%d\n", sizeof(long long));
+	//printf("%d\n", sizeof(float));
+	//printf("%d\n", sizeof(double));
+	//return 0;
+
+	//extern int g_val;
+	//printf("%d\n", g_val);
+
+	//const int num = 4; //常变量
+	//#define 标识符常量+
+	return 0;
 }
